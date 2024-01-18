ad92e6e13ebde89248852e8bedd0e335ab2dffaa
Petr Hejduk <hejdyson@gmail.com>
2024-01-18 23:34:12 +0100

    added separate classes for line, underlying and stone

--- visuals/button.py
+++ visuals/button.py
@@ -1,7 +1,8 @@
 import pygame
 
+
 # button class
-class Button():
+class Line():
     def __init__(self, player_index, stone_pos, name, x, y, image, image_hover, width_scale, height_scale):
         self.player_index = player_index
         self.stone_pos = stone_pos
@@ -30,7 +31,6 @@ class Button():
                 self.clicked = True
                 action = True
         
-
         # if button is not clicked - goes back to false
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
@@ -42,3 +42,89 @@ class Button():
             surface.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
+
+
+
+class Underlying():
+    def __init__(self, player_index, stone_pos, name, x, y, image, image_hover, width_scale, height_scale):
+        self.player_index = player_index
+        self.stone_pos = stone_pos
+        self.name = name
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width*width_scale), int(height*height_scale)))
+        self.image_hover = pygame.transform.scale(image_hover, (int(width*width_scale), int(height*height_scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topright = (x, y)
+        self.clicked = False
+        self.hovered = False
+        # list for stones on underlying
+        self.stones = []
+    
+    def draw(self, surface):
+        action = False
+        self.hovered = False
+        
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            self.hovered = True
+            # check if clicked
+            if pygame.mouse.get_pressed()[0] and self.clicked == False:
+                self.clicked = True
+                action = True
+    
+        # if button is not clicked - goes back to false
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button on screen
+        if self.hovered:
+            surface.blit(self.image_hover, (self.rect.x, self.rect.y))
+        else:
+            surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+    
+
+
+class Stone():
+    def __init__(self, player_index, stone_pos, name, x, y, image, image_hover, width_scale, height_scale):
+        self.player_index = player_index
+        self.stone_pos = stone_pos
+        self.name = name
+        self.x = x
+        self.y = y
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width*width_scale), int(height*height_scale)))
+        self.image_hover = pygame.transform.scale(image_hover, (int(width*width_scale), int(height*height_scale)))
+
+        self.clicked = False
+        self.hovered = False
+    
+
+    def draw(self, surface):
+        self.rect = self.image.get_rect()
+        self.rect.topright = (self.x, self.y)
+
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            # check if clicked
+            if pygame.mouse.get_pressed()[0] and self.clicked == False:
+                self.clicked = True
+        
+
+        # if button is not clicked - goes back to false
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return
