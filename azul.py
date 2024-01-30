9f5f4941ef8468aa57a81558e0ab2f6267e7a7ff
Petr Hejduk <hejdyson@gmail.com>
2024-01-30 11:48:03 +0100

    created separate file for backend functions, adjusted imports

--- azul.py
+++ azul.py
@@ -1,108 +1,12 @@
-from random import shuffle
-
 from classes.board import Board
-from classes.player import Player
+import backend
 
 
-
-# create players with generic names
-def create_players(board):
-    for i in range(board.number_of_players):
-        player = Player('Player ' + str(i + 1))
-        player.position_on_board = i + 1
-        board.list_of_players.append(player)
-    
-    for player in board.list_of_players:
-        print('player name', player.name)
-
-# choose player order - starts player with first player = True
-def choose_player_order(board):
-    # first round - random order
-    if board.round_counter == 1:
-        shuffle(board.list_of_players)
-        for player in board.list_of_players:
-            print('player ', player.name, 'position on board', player.position_on_board)
-        cont = int(input('after shuffle'))
-    else:
-        index = 0
-        start_appending = False
-        player_order = []
-        # creating looped list 1 -> 2 -> 3 -> 4 -> 1   - first will be the one with first player mark - every round this can change
-        while True:
-            if board.list_of_players[index].first_player == True:
-                start_appending = True
-            if start_appending:
-                player_order.append(board.list_of_players[index])
-            if index + 1 == len(board.list_of_players):
-                index = -1
-            if len(player_order) == len(board.list_of_players):
-                break
-
-            index += 1
-        board.list_of_players.clear()
-        for i in range(len(player_order)):
-            board.list_of_players.append(player_order[i])
-        for player in board.list_of_players:
-            print('player ', player.name)
-        player_order.clear()
-
-
-# Displaying final score
-def display_final_score(board):
-    players_to_sort = []
-    for player in board.list_of_players:
-        players_to_sort.append(player)
-    players_sorted = sorted(players_to_sort, key=lambda player: player.points_total, reverse=True)
-    print('')
-    print('----------------------------')
-    print('The Winner is: ', players_sorted[0].name)
-    print('----------------------------')
-    print()
-    print('Final score:')
-    print('----------------------------')
-    print('Player Name\tTotal Points')
-    print('-------------+--------------')
-    for player in players_sorted:
-        print(player.name, '\t', player.points_total)
-    
-    display_stats_from_rounds(players_sorted)
-
-# Displaying stats from all rounds
-def display_stats_from_rounds(players_sorted):
-    print()
-    print('Stats from all rounds:')
-    print('----------------------')
-    # header print
-    print('Round   ', end='\t')
-    for player in players_sorted:
-        print(player.name, end='\t')
-    print()
-    # first for loop - number of rounds (printing rows)
-    for round_index in range(len(players_sorted[0].list_of_points_from_rounds)):
-        print('Round', round_index + 1, end='\t\t')
-        # for every player print points from each round (columns)
-        for player in players_sorted:
-            print(player.list_of_total_points[round_index], ' (', player.list_of_points_from_rounds[round_index], ') ', end='\t')
-        print()
-    print('Ending points', end='\t')
-    for player in players_sorted:
-        print(player.ending_points, end='\t\t')
-    print()
-    print('-'*60)
-    # printing total points from the end
-    print('Total', end='\t\t')
-    sum_points_total = 0
-    for player in players_sorted:
-        print(player.points_total, end='\t\t')
-        sum_points_total += player.points_total
-    print('||', sum_points_total, '||')
-
-            
 def main():
     brd = Board()
 
     brd.select_num_players()
-    create_players(brd)
+    backend.create_players(brd)
     brd.draw_underlyings()
     brd.append_bag_of_tiles()
     #print('after addition', brd.bag_of_tiles)
@@ -113,11 +17,11 @@ def main():
         brd.fill_in_underlyings()
         # Assign player order
         print('choosing order')
-        choose_player_order(brd)
+        backend.choose_player_order(brd)
 
         for index, player in enumerate(brd.list_of_players):
             print('index:', index, 'Player:', player.name, 'first player', player.first_player, 'position on board', player.position_on_board)
-        cont = int(input('cont?'))
+        # cont = int(input('cont?'))
         # REMOVING FIRST PLAYER MARK
         for player in brd.list_of_players:
             if player.first_player == True:
@@ -129,9 +33,10 @@ def main():
             player = brd.list_of_players[player_index]
             print()
             print('Player on turn:', player.name)
+
             player.print_player_table()
-            if brd.round_counter == 2:
-                cont = int(input('cont?'))
+            # if brd.round_counter == 2:
+                # cont = int(input('cont?'))
             print()
             player.choose_tile(brd)
             print('underlyings', brd.underlyings)
@@ -145,7 +50,6 @@ def main():
         # COUNTING POINTS AFTER END OF A ROUND
         game_over = False
         for player in brd.list_of_players:
-            print()
             print('all underlyings empty - end of round. Start counting points..')
             player.print_player_table()
             player.place_all_tiles_to_right(brd)
@@ -171,7 +75,7 @@ def main():
             for player in brd.list_of_players:
                 player.print_player_table()
                 player.count_ending_points()
-            display_final_score(brd)
+            backend.display_final_score(brd)
             break
         
         # another = int(input('Continue?'))
