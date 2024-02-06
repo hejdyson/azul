2b252c19cff1a25fe6114e8b2bfc884f6732b848
Petr Hejduk <hejdyson@gmail.com>
2024-02-06 22:58:15 +0100

    added rest of server

--- server.py
+++ server.py
@@ -17,7 +17,24 @@ print('Waiting for a connection, Server Started')
 
 
 def threaded_client(conn):
-    pass
+
+    reply = ''
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+
+            if not data:
+                print('Disconnected')
+                break
+            else:
+                print('Received: ', reply)
+                print('Sending: ', reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
 
 while True:
     conn, addr = s.accept()
